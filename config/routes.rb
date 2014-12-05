a42e41c5c9f868874cc78b6034592c2003754194
Adam Legg <adam.legg@gmail.com>
2014-12-05 11:30:50 -0700

    banner functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
- namespace :api do
-  namespace :v1 do
-    resources :leads
+ root to: 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :leads
+    end
   end
-end
 
   get '*path', to: 'home#index'
- end
+
+end
